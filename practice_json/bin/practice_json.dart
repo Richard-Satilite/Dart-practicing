fdba0e39d13269995473b715b533706f063760f9
Richard <richardsatilite.dev@gmail.com>
2024-12-24 11:32:33 -0300

    Dart-practice: First commit

---
+++ practice_json/bin/practice_json.dart
@@ -0,0 +1,5 @@
+import 'package:practice_json/practice_json.dart' as practice_json;
+
+void main(List<String> arguments) {
+  print('Hello world: ${practice_json.calculate()}!');
+}
---
+++ practice_json/test/practice_json_test.dart
@@ -0,0 +1,8 @@
+import 'package:practice_json/practice_json.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
