f70b9b5cf53aaa4498d5e15a3871bc599bef3eea
Richard <richardsatilite.dev@gmail.com>
2024-12-24 11:35:46 -0300

    Dart-practice -> json: added .keepGit files

--- practice_json/bin/practice_json.dart
+++ practice_json/bin/practice_json.dart
@@ -1,5 +1,1 @@
-import 'package:practice_json/practice_json.dart' as practice_json;
 
-void main(List<String> arguments) {
-  print('Hello world: ${practice_json.calculate()}!');
-}
--- practice_json/test/practice_json_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:practice_json/practice_json.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
